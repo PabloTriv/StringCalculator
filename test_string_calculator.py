68ffedece775fc2795b9ac2dcd752fbee4d4aefd
Pablo <pablotrivino999@gmail.com>
2021-07-27 11:25:30 -0400

    Added second test

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -8,3 +8,13 @@ def test_correct_params_expect_OP_NUM_NUM():
     result = cut.parse(inputVal)
     # Assert
     assert result == expResult
+
+def test_incorrect_operator_expect_INVOP_NUM_NUM():
+    # Arrange
+    cut = string_calculator.StringCalculator()
+    inputVal = "1, 3, 4"
+    expResult = ["INVALID_OPERATOR", "NUMBER", "NUMBER"]
+    # Act
+    result = cut.parse(inputVal)
+    # Assert
+    assert result == expResult
